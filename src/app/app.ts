c5d583c616d000c9e85e0fb481cd3b8bec025531
Sergio Mancilla <serx.athom@gmail.com>
2025-12-22 00:59:05 -0500

    Integrate and test mc-kit library

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,39 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, Signal, signal } from '@angular/core';
+import { MCTable } from '@mckit/table';
+import { MCColumn, MCListResponse } from '@mckit/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MCTable],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('tots-frontend');
+
+  columns: MCColumn[] = [
+    {
+      title: 'Name',
+      field: 'name',
+      isShow: true,
+      // isSortable: true,
+      // isSortDefault: false,
+      // isSortDescDefault: false
+    }
+  ]
+
+  ngOnInit(): void {
+    this.response.data = [{ name: 'Sergio' }, { name: 'Juan' }]
+    this.response.total = 2
+    this.response.current_page = 1
+    this.response.per_page = 50
+    this.response.from = 1
+    this.response.to = 2
+  }
+
+  response = new MCListResponse<{ name: string }>()
+
+  onPage(event: any) {
+    console.log(event)
+  }
 }
