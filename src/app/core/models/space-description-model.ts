0686647b191401cb4e0d96de14ca473847b2aa37
Sergio Mancilla <serx.athom@gmail.com>
2026-01-06 23:49:33 -0500

    Change spaces enum

--- src/app/core/models/space-description-model.ts
+++ src/app/core/models/space-description-model.ts
@@ -4,10 +4,8 @@ type SpaceDescription = {
 };
 
 export const SPACE_TYPES: SpaceDescription[] = [
-  { value: 'conference_room', label: 'Conference Room' },
-  { value: 'huddle_room', label: 'Huddle Room' },
-  { value: 'private_office', label: 'Private Office' },
-  { value: 'hot_desk', label: 'Hot Desk' },
-  { value: 'phone_booth', label: 'Phone Booth' },
-  { value: 'training_room', label: 'Training Room' },
+  { value: 'auditorio', label: 'Auditorio' },
+  { value: 'sala_de_reuniones', label: 'Sala de Reuniones' },
+  { value: 'oficina', label: 'Oficina' },
+  { value: 'laboratorio ', label: 'Laboratorio' },
 ] as const;
